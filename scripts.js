ac58c207a0550cce5cce2d0b996c3bf5cf7d8840
akosstv <akosstv@users.noreply.github.com>
2020-04-30 18:42:09 +0200

    add last for today

--- scripts.js
+++ scripts.js
@@ -11,7 +11,13 @@ $('h2').toggleClass('blue') //masodik eltunteti amit az elso rarak
 
 
 function myLittleFunction() {
-    $('main').append('<p> A p is appended in the main</p>');
+    //condition: number of ps is smaller than 5
+    if ($('p').length<5) {
+        $('main').append('<p> A p is appended in the main</p>');
+    } else {
+        console.log('We have reached the maximum paragraph number');
+    }
+    
     $('p').css('border', '1px solid red');
     $('p').css('padding', '14px');
 }
@@ -22,6 +28,16 @@ $('#add').click(myLittleFunction);
 $('#remove').click(function () {
     $('p:last-child').remove();
 });
+
+//Forth part:
+
+//if (condition that true or false) {
+    //cond is true
+//} else {
+    //cond is false
+//}
+
+let colors = ['orange', 'yellow', 'blue', 'lime', 'magenta']
 
 
 // Single line comment
