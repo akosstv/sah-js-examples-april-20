709caa71daeb8e05b313af961df067a4d7640585
akosstv <akosstv@users.noreply.github.com>
2020-04-30 18:31:34 +0200

    add third part

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
-//Third part:
-//$('css selector').action()
-$('h1').html('This is done by <em>JavaScript</em>!');
-$('p').css('border', '1px solid red')
-$('p').css('padding', '14px')
-$('h2').toggleClass('blue')
-$('h2').toggleClass('blue')
 
-console.log('Script file is loaded.')
+//Third part:
+console.log('Script file is loaded.');
+
+$('h1').html('This is done by <em>JavaScript</em>!');
+ //$('css selector').action()
+
+$('h2').toggleClass('blue');
+$('h2').toggleClass('blue') //masodik eltunteti amit az elso rarak
+
+
+
+function myLittleFunction() {
+    $('main').append('<p> A p is appended in the main</p>');
+    $('p').css('border', '1px solid red');
+    $('p').css('padding', '14px');
+}
+
+myLittleFunction();
+
+$('#add').click(myLittleFunction);
+$('#remove').click(function () {
+    $('p:last-child').remove();
+});
+
 
 // Single line comment
 
