9f1f4bf1d0dc79f709b0556ebaf6197f6c688342
akosstv <akosstv@users.noreply.github.com>
2020-04-30 18:09:31 +0200

    add jquery first few examples

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+//Third part:
+//$('css selector').action()
+$('h1').html('This is done by <em>JavaScript</em>!');
+$('p').css('border', '1px solid red')
+$('p').css('padding', '14px')
+$('h2').toggleClass('blue')
+$('h2').toggleClass('blue')
+
 console.log('Script file is loaded.')
 
 // Single line comment
